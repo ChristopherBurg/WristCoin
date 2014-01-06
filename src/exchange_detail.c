139065da0c329077d6722806586888f983df5343
Christopher Burg <christopherburg@christopherburg.com>
2014-01-05 22:20:10 -0600

    Added feature to scroll through extended data.
    The extended data window now contains a scroll layer so the user can
    scroll through the difference extended data.

--- src/exchange_detail.c
+++ src/exchange_detail.c
@@ -1,6 +1,8 @@
 #include "exchange_detail.h"
 
 static Window *window;
+static ScrollLayer *scroll_layer;
+
 static TextLayer *exchange_name_text;
 static TextLayer *low_label;
 static TextLayer *low_text;
@@ -33,79 +35,99 @@ static void window_load(Window *window) {
     GRect bounds = layer_get_bounds(window_layer);
     int half_screen = (bounds.size.w / 2);
 
-    exchange_name_text = text_layer_create((GRect) { .origin = {0, 0}, .size = {bounds.size.w, 32 } });
+    scroll_layer = scroll_layer_create((GRect) { .origin = { 0, 32 }, .size = { bounds.size.w, bounds.size.h - 32 } });
+    scroll_layer_set_content_size(scroll_layer, GSize(bounds.size.w, 146));
+    layer_add_child(window_layer, scroll_layer_get_layer(scroll_layer));
+
+    exchange_name_text = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, 32 } });
     text_layer_set_text_alignment(exchange_name_text, GTextAlignmentCenter);
     text_layer_set_font(exchange_name_text, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
     layer_add_child(window_layer, text_layer_get_layer(exchange_name_text));
+//    scroll_layer_add_child(scroll_layer, text_layer_get_layer(exchange_name_text));
 
     // Label and text for the low value.
-    low_label = text_layer_create((GRect) { .origin = { 0, 32 }, .size = { half_screen, 22 } });
+    low_label = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { half_screen, 22 } });
     text_layer_set_font(low_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text(low_label, "Low:");
-    layer_add_child(window_layer, text_layer_get_layer(low_label));
+//    layer_add_child(window_layer, text_layer_get_layer(low_label));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(low_label));
 
-    low_text = text_layer_create((GRect) { .origin = { half_screen, 32 }, .size = { half_screen, 22 } });
+    low_text = text_layer_create((GRect) { .origin = { half_screen, 0 }, .size = { half_screen, 22 } });
     text_layer_set_font(low_text, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text(low_text, low);
-    layer_add_child(window_layer, text_layer_get_layer(low_text));
+//    layer_add_child(window_layer, text_layer_get_layer(low_text));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(low_text));
 
     // Label and text for the high value.
-    high_label = text_layer_create((GRect) { .origin = { 0, 54 }, .size = { half_screen, 22 } });
+    high_label = text_layer_create((GRect) { .origin = { 0, 22 }, .size = { half_screen, 22 } });
     text_layer_set_font(high_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text(high_label, "High:");
-    layer_add_child(window_layer, text_layer_get_layer(high_label));
+//    layer_add_child(window_layer, text_layer_get_layer(high_label));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(high_label));
 
-    high_text = text_layer_create((GRect) { .origin = { half_screen, 54 }, .size = { half_screen, 22 } });
+    high_text = text_layer_create((GRect) { .origin = { half_screen, 22 }, .size = { half_screen, 22 } });
     text_layer_set_font(high_text, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text(high_text, high);
-    layer_add_child(window_layer, text_layer_get_layer(high_text));
+//    layer_add_child(window_layer, text_layer_get_layer(high_text));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(high_text));
 
     // Label and text for the last value.
-    last_label = text_layer_create((GRect) { .origin = { 0, 76 }, .size = { half_screen, 22 } });
+    last_label = text_layer_create((GRect) { .origin = { 0, 44 }, .size = { half_screen, 22 } });
     text_layer_set_font(last_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text(last_label, "Last:");
-    layer_add_child(window_layer, text_layer_get_layer(last_label));
+//    layer_add_child(window_layer, text_layer_get_layer(last_label));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(last_label)); 
 
-    last_text = text_layer_create((GRect) { .origin = { half_screen, 76 }, .size = { half_screen, 22 } });
+    last_text = text_layer_create((GRect) { .origin = { half_screen, 44 }, .size = { half_screen, 22 } });
     text_layer_set_font(last_text, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text(last_text, last);
-    layer_add_child(window_layer, text_layer_get_layer(last_text));
+//    layer_add_child(window_layer, text_layer_get_layer(last_text));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(last_text));
 
     // Label and text for the average value.
-    average_label = text_layer_create((GRect) { .origin = { 0, 98 }, .size = { half_screen, 22 } });
+    average_label = text_layer_create((GRect) { .origin = { 0, 66 }, .size = { half_screen, 22 } });
     text_layer_set_font(average_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text(average_label, "Average:");
-    layer_add_child(window_layer, text_layer_get_layer(average_label));
+//    layer_add_child(window_layer, text_layer_get_layer(average_label));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(average_label));
 
-    average_text = text_layer_create((GRect) { .origin = { half_screen, 98 }, .size = { half_screen, 22 } });
+    average_text = text_layer_create((GRect) { .origin = { half_screen, 66 }, .size = { half_screen, 22 } });
     text_layer_set_font(average_text, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text(average_text, average);
-    layer_add_child(window_layer, text_layer_get_layer(average_text));
+//    layer_add_child(window_layer, text_layer_get_layer(average_text));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(average_text));
 
     // Label and text for the buy value.
-    buy_label = text_layer_create((GRect) { .origin = { 0, 120 }, .size = { half_screen, 22 } });
+    buy_label = text_layer_create((GRect) { .origin = { 0, 88 }, .size = { half_screen, 22 } });
     text_layer_set_font(buy_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text(buy_label, "Buy:");
-    layer_add_child(window_layer, text_layer_get_layer(buy_label));
+//    layer_add_child(window_layer, text_layer_get_layer(buy_label));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(buy_label));
 
-    buy_text = text_layer_create((GRect) { .origin = { half_screen, 120 }, .size = { half_screen, 22 } });
+    buy_text = text_layer_create((GRect) { .origin = { half_screen, 88 }, .size = { half_screen, 22 } });
     text_layer_set_font(buy_text, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text(buy_text, buy);
-    layer_add_child(window_layer, text_layer_get_layer(buy_text));
+//    layer_add_child(window_layer, text_layer_get_layer(buy_text));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(buy_text));
 
     // Label and text for the sell value.
-    sell_label = text_layer_create((GRect) { .origin = { 0, 142 }, .size = { half_screen, 22 } });
+    sell_label = text_layer_create((GRect) { .origin = { 0, 110 }, .size = { half_screen, 22 } });
     text_layer_set_font(sell_label, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
     text_layer_set_text(sell_label, "Sell:");
-    layer_add_child(window_layer, text_layer_get_layer(sell_label));
+//    layer_add_child(window_layer, text_layer_get_layer(sell_label));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(sell_label));
 
-    sell_text = text_layer_create((GRect) { .origin = { half_screen, 142 }, .size = { half_screen, 22 } });
+    sell_text = text_layer_create((GRect) { .origin = { half_screen, 110 }, .size = { half_screen, 22 } });
     text_layer_set_font(sell_text, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text(sell_text, sell);
-    layer_add_child(window_layer, text_layer_get_layer(sell_text));
+//    layer_add_child(window_layer, text_layer_get_layer(sell_text));
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(sell_text));
+
+    scroll_layer_set_click_config_onto_window(scroll_layer, window);
 }
 
 static void window_unload(Window *window) {
+    scroll_layer_destroy(scroll_layer);
     text_layer_destroy(exchange_name_text);
     text_layer_destroy(last_text);
 }
